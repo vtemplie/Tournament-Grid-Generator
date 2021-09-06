c90258f5e67f4605199b0180262928d9e752986f
Vincent Templier <vincent.templier.05@gmail.com>
2021-09-06 21:41:47 +0200

    Add structures to design and build the grid

--- include/typedefs.h
+++ include/typedefs.h
@@ -17,32 +17,62 @@
 #define _TYPEDEFS_H
 
 #define NAME_MAX_LENGTH 20
-#define NUM_MIN_PLAYERS 2
-#define NUM_MAX_PLAYERS 20
+#define NB_MIN_PLAYERS 2
+#define NB_MAX_PLAYERS 20
 
-/** Enumeration for containing the types of display style */
+
+/** Enumeration for the types of display style */
 typedef enum {
     STYLE_COMPACT,
     STYLE_TABLE
-} Style;
+} Style_t;
 
-/** Enumeration for containing the types of display format */
+
+/** Enumeration for the types of display format */
 typedef enum {
     FORMAT_ID,
     FORMAT_NAME
-} Format;
+} Format_t;
+
+
+/** Enumeration for the types of errors */
+typedef enum {
+    NO_ERROR,
+    WRONG_NB_ARG,
+    UNKNOWN_OPTION,
+    UNKNOWN_STYLE,
+    UNKNOWN_FORMAT,
+    LIMIT_MIN_PLAYERS,
+    LIMIT_MAX_PLAYERS
+} Error_t;
+
 
 /** Object for containing the list of players */
 typedef struct {
-    unsigned int num_players;
-    char name[NUM_MAX_PLAYERS][NAME_MAX_LENGTH + 1];
+    unsigned int nb_players;
+    char name[NB_MAX_PLAYERS][NAME_MAX_LENGTH + 1];
 } PlayerList;
 
-/** Object for designing the tournament grid */
+
+/** Object for containing the tournament grid */
 typedef struct {
-    unsigned int num_players;
-    unsigned int day[NUM_MAX_PLAYERS][NUM_MAX_PLAYERS];
+    unsigned int nb_players;
+    unsigned int day[NB_MAX_PLAYERS][NB_MAX_PLAYERS];
 } Grid;
+
+
+/** Object for containing the details of the error that occurred */
+typedef struct {
+    Error_t error_type;     /* Error type */
+    char* msg;              /* Additional information about the error */ 
+} ErrorMessage;
+
+
+/** Object for containing the design of the grid */
+typedef struct {
+    Format_t format;
+    Style_t style;
+} GridDesign;
 
 
 
